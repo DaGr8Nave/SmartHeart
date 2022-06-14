ea906ac45da479067138598345e12cf621520a53
Evan Wen <evan.h.wen@gmail.com>
2022-06-14 01:22:57 -0400

    discriminator/generator for cyclegan

---
+++ models/CycleGAN/generator_model.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+	def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwargs):
+		super().__init__()
+		self.conv = nn.Sequential(
+			nn.Conv1d(in_channels, out_channels, padding_mode = 'reflect', **kwargs)
+			if down
+			else nn.ConvTranspose1d(in_channels, out_channels, **kwargs),
+			nn.InstanceNorm1d(out_channels),
+			nn.ReLU(inplace=True) if use_act else nn.Identity()
+		)
+	def forward(self, x):
+		return self.conv(x)
+
+class ResidualBlock(nn.Module):
+	def __init__(self, channels):
+		super().__init__()
+		self.block = nn.Sequential(
+			ConvBlock(channels, channels, kernel_size=3, padding=1),
+			ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+		)
+	def forward(self, x):
+		return x + self.block(x)
+
+class Generator(nn.Module):
+	def __init__(self, in_channels=12, num_features = 64, num_residuals=9):
+		super().__init__()
+		self.initial = nn.Sequential(
+			nn.Conv1d(in_channels, num_features, kernel_size=7, stride=1, padding=3, padding_mode='reflect'),
+			nn.ReLU(inplace=True),
+		)
+		self.down_blocks = nn.ModuleList(
+			[
+				ConvBlock(64, num_features*2, kernel_size=3, stride=2, padding=1),
+				ConvBlock(num features*2, num_features*4, kernel_size=3, stride=2, padding=1),
+			]
+		)
+		self.residual_blocks = nn.Sequential(
+			*[ResidualBlock(num_features*4) for _ in range(num_residuals)]
+		)
+		self.up_blocks = nn.ModuleList(
+			[
+				ConvBlock(num_features*4, num_features*2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+				ConvBlock(num_features*2, num_features*1, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+			]
+		)
+
+		self.last = nn.Conv1d(num_features, in_channels, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
